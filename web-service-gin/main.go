5e11d938dbdf5bf153650d18407c76d72a8c8df9
Alex Martinez <alexmartinez0071234@gmail.com>
2022-08-04 21:21:40 -0400

    post req works

--- web-service-gin/main.go
+++ web-service-gin/main.go
@@ -25,7 +25,8 @@ var albums = []album{
 func main() {
     router := gin.Default()
     router.GET("/albums", getAlbums)
-    router.GET("albums/:id", getAlbumByID)
+    router.GET("/albums/:id", getAlbumByID)
+    router.POST("/albums", postAlbums)
 
     router.Run("localhost:3001")
 }
@@ -49,4 +50,18 @@ func getAlbumByID(c *gin.Context){
         }
     }
     c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+}
+
+// postAlbums adds an album from JSON received in the request body.
+func postAlbums (c * gin.Context) {
+    var newAlbum album
+
+    // Call BindJSON to bind the received JSON to newAlbum. 
+    if err := c.BindJSON(&newAlbum); err != nil {
+        return
+    }
+    // Add the new album to the slice.
+    albums = append(albums, newAlbum)
+
+    c.IndentedJSON(http.StatusCreated, newAlbum)
 }
